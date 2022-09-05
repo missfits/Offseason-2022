05a6ff973864afee32b9aa8408743cbd7b5f2e51
Karina Anders <67711281+kmanders317@users.noreply.github.com>
2022-09-05 10:09:22 -0700

    Added conveyor subsystem and command

---
+++ src/main/java/frc/robot/commands/ConveyorCommand.java
@@ -0,0 +1,43 @@
+package frc.robot.commands;
+
+import static frc.robot.Constants.*;
+
+import frc.robot.subsystems.Conveyor;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.SensorBoard;
+import frc.robot.OI;
+
+
+public class ConveyorCommand extends CommandBase{
+    
+    Conveyor m_conveyor;
+    SensorBoard m_sensorControl;
+    OI m_humanControl;
+
+    public ConveyorCommand(SensorBoard sensorControl, OI humanControl, Conveyor conveyor) {
+        m_sensorControl = sensorControl;
+        m_humanControl = humanControl;
+        m_conveyor = conveyor;
+    }
+
+    @Override
+    public void initialize() {
+
+    }
+
+    @Override
+    public void execute() {
+       m_conveyor.setConveyorPower(0.5);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        m_conveyor.setConveyorPower(0.0);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return !(m_humanControl.isDown(m_humanControl.getDesiredButton(kControllerID_XBOX, kButtonID_XboxLB))); 
+    }
+}
+
