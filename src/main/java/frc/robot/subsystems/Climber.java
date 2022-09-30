11b259713fd60395b0b894bb58facf69fa974a31
Karina Anders <67711281+kmanders317@users.noreply.github.com>
2022-09-29 19:01:48 -0700

    Created climber file

---
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -0,0 +1,22 @@
+// package frc.robot.subsystems;
+
+// import com.revrobotics.CANSparkMax;
+// import com.revrobotics.RelativeEncoder;
+
+// import frc.robot.SensorBoard;
+
+// public class Climber {
+//     private SensorBoard m_sensorControl;
+
+//     public final CANSparkMax m_conveyorMotor;
+//     public final RelativeEncoder m_conveyorEncoder;
+
+//     public (SensorBoard sensorBoard) {
+//         m_sensorControl = sensorBoard;
+//         m_conveyorMotor = new CANSparkMax(kCANID_MotorConveyor, MotorType.kBrushless);
+//         m_conveyorEncoder = m_conveyorMotor.getEncoder();
+//         m_conveyorMotor.setInverted(true);
+
+
+//     }
+// }
